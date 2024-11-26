b5be1a6a8fa551df60364d0001b45fc184c60efc
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-11-26 13:40:54 +0100

    Initial commit

---
+++ src/main/java/org/example/backendwakandatransportemovilidad/BackendWakandaTransporteMovilidadApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandatransportemovilidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWakandaTransporteMovilidadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWakandaTransporteMovilidadApplication.class, args);
+    }
+
+}
